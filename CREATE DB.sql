b0506809affd5e2d8a942cec5c23eba771b144a3
aWxr <alfieward100@gmail.com>
2025-07-01 15:06:13 +0100

    Updated SQL script and added legality callout to readme

--- CREATE DB.sql
+++ CREATE DB.sql
@@ -1,5 +1,5 @@
 -- Drop the database:
-DROP DATABASE IF EXISTS ETLAssignment;
+--DROP DATABASE IF EXISTS ETLAssignment;
 
 -- Drop tables in case of issue:
 DROP TABLE ;
@@ -7,3 +7,15 @@ DROP TABLE ;
 
 -- Create database:
 CREATE DATABASE ETLAssignment;
+
+CREATE TABLE air_quality_tb(
+	id INT PRIMARY KEY,  
+	location varchar(90),
+	location_type varchar(22) type,
+	latitude_normalised decimal(20, 2),
+	longitude_normalised decimal(20, 2),
+	pollutants varchar(38)
+);
+
+-- Display table:
+SELECT * FROM air_quality_tb;
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 255003-green-data-project
 Green Data Analytics project for Data Engineering course
+
+Contains public sector information licensed under the Open Government Licence v3.0.
